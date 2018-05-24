210f4a7f5783421dc096b69fb355acc80d67219d
Emil Esplund <eesplund@gmail.com>
2018-05-24 16:06:54 +0200

    Added login button in header and fixed main styling for body content

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="./assets/styles/styles.css" rel="stylesheet">
 </head>
-<body class="color-body">
+<body>
 	<header>
 		<div class="header wrapper">
 			<div class="header__logo-holder">
@@ -22,11 +22,17 @@
 					<button><i class="fa fa-search"></i></button>
 				</div>
 			</div>
-			
+			<div class="header__user">
+				<div class="header__login">
+					<a href="#" id="login-btn">Logga in</a>
+				</div>
+			</div>
 		</div>
 	</header>
-	<div class="container wrapper--half color-body">
-		<h1>Kursböcker med bästa omdömme!</h1>
+	<div class="container">
+		<div class="wrapper--half">
+			<h1 class="section__title">Kursböcker med bästa omdöme!</h1>
+		</div>
 	</div>
 </body>
 </html>
