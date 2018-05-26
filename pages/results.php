e959947c589b83457ed201b624ec17f294aed915
Emil Esplund <eesplund@gmail.com>
2018-05-26 12:51:49 +0200

    Adjusted some of the css for the search result page

--- pages/results.php
+++ pages/results.php
@@ -37,10 +37,11 @@
 	</header>
 	<div class="container">
 		<div class="wrapper__body">
-			<h1 class="section__title section__title--smaller section__title--left">Sökresultat för "System"</h1>
 			<div class="section__row">
-				<div class="section__col-6 section__col--left-content">
-
+				<h1 class="section__title section__title--smaller section__title--left">Sökresultat för "System"</h1>
+			</div>
+			<div class="section__row">
+				<div class="section__col-m-6">
 					<div class="section__book">
 						<div class="img__book">
 							<a href="#">
@@ -97,12 +98,65 @@
 							</div>
 						</div>
 					</div>
-
-
-
-
 				</div>
+				<div class="section__col-m-6">
+					<div class="section__book">
+						<div class="img__book">
+							<a href="#">
+								<img src="../assets/images/systems-analysis.jpg" class="img__book--small">
+							</a>
+						</div>
+						<div class="section__bookinfo">
+							<h1 class="label--smaller">Systems Analysis and Design</h1>
+							<p class="label--tiny">2013</p>
+							<div class="rating__small">
+								<span class="stars__star fa fa-star stars__star--checked"></span>
+								<span class="stars__star fa fa-star stars__star--checked"></span>
+								<span class="stars__star fa fa-star stars__star--checked"></span>
+								<span class="stars__star fa fa-star stars__star--checked"></span>
+								<span class="stars__star fa fa-star"></span>
+							</div>
+						</div>
+					</div>
 
+					<div class="section__book">
+						<div class="img__book">
+							<a href="#">
+								<img src="../assets/images/business-information-systems.jpg" class="img__book--small">
+							</a>
+						</div>
+						<div class="section__bookinfo">
+							<h1 class="label--smaller">Business Information Systems</h1>
+							<p class="label--tiny">2013</p>
+							<div class="rating__small">
+								<span class="stars__star fa fa-star stars__star--checked"></span>
+								<span class="stars__star fa fa-star stars__star--checked"></span>
+								<span class="stars__star fa fa-star "></span>
+								<span class="stars__star fa fa-star "></span>
+								<span class="stars__star fa fa-star"></span>
+							</div>
+						</div>
+					</div>
+
+					<div class="section__book">
+						<div class="img__book">
+							<a href="#">
+								<img src="../assets/images/how-to-think-like-a-programmer.jpg" class="img__book--small">
+							</a>
+						</div>
+						<div class="section__bookinfo">
+							<h1 class="label--smaller">How to THINK like a programmer</h1>
+							<p class="label--tiny">2013</p>
+							<div class="rating__small">
+								<span class="stars__star fa fa-star stars__star--checked"></span>
+								<span class="stars__star fa fa-star"></span>
+								<span class="stars__star fa fa-star"></span>
+								<span class="stars__star fa fa-star"></span>
+								<span class="stars__star fa fa-star"></span>
+							</div>
+						</div>
+					</div>
+				</div>
 		</div>
 	</div>
 </body>
