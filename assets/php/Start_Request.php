d89e0dad5c6e43f0c7a6fd8a654abc7a994fb3fb
KalladAmos <Kalladamos@gmail.com>
2018-05-27 18:55:15 +0200

    i dont even know anymore...

--- assets/php/Start_Request.php
+++ assets/php/Start_Request.php
@@ -1,10 +1,51 @@
 <?php
 require('connect.php');
+require('Review_AVG.php');
 
-$get_avg_and_ISBN = $connection -> query("SELECT AVG(rank), ISBN FROM rank GROUP BY ISBN ORDER BY `AVG(rank)` DESC LIMIT 3");
-$get_ISBN = $connection -> query("SELECT ISBN FROM '$get_avg_and_ISBN'");
-$get_title  = $connection -> query("SELECT title FROM book_table WHERE ISBN = '$get_ISBN'");
-$get_picture = $connection -> query("SELECT picture FROM book_table WHERE book_ISBN = '$get_ISBN'");
+//$get_avg_and_ISBN = $connection -> query("SELECT AVG(rank), book_ISBN FROM rank GROUP BY ISBN ORDER BY `AVG(rank)` DESC LIMIT 3");
+
+$get_ISBN = $connection -> query("SELECT book_ISBN FROM '$get_review_AVG' ORDER BY `AVG(rating_by_user)` LIMIT 3 ");
+
+$get_title  = $connection -> query("SELECT book_title FROM book_table WHERE book_ISBN = '$get_ISBN'");
+
+//$get_title  = $connection -> query("SELECT title FROM book_table JOIN book_table ON book_table.ISBN = '$get_ISBN'-book_ISBN");
+
+$get_picture = $connection -> query("SELECT book_picture FROM book_table WHERE book_ISBN = '$get_ISBN'");
+
+//$get_picture = $connection -> query("SELECT book_picture FROM book_table JOIN book_table ON book_table.book_ISBN = '$get_ISBN'.book_table");
+
+
+
+	if($get_ISBN === FALSE || $get_title === FALSE) { 
+    die(mysql_error());
+	}
+
+		
+	//	while($row = mysqli_fetch_array($get_avg_and_ISBN))
+	//	{
+  	//	$AVG_rank=$row['AVG(rank)'];	
+	//	echo $AVG_rank '<br />';
+	//	}
+	
+		
+		while($row = mysqli_fetch_array($get_ISBN))
+		{
+  		$ISBN=$row['book_ISBN'];	
+		echo $ISBN'<br />';
+		}
+		
+		
+		while($row = mysqli_fetch_array($get_title))
+		{
+  		$title=$row['book_title'];	
+		echo $title'<br />';
+		}
+
+		while($row = mysqli_fetch_array($get_picture))
+		{
+  		$picture=$row['book_picture'];	
+		echo $picture'<br />';
+		}
 
 
 ?>
---
+++ assets/php/start_book.php
@@ -0,0 +1,41 @@
+
+				<div class="section__col-4 section__col--centered-content">
+					<a href="#">
+						<img src="./assets/images/systems-analysis.jpg" class="img__book--medium">
+					</a>
+					<div class="rating">
+						<span class="stars__star fa fa-star stars__star--checked"></span>
+						<span class="stars__star fa fa-star stars__star--checked"></span>
+						<span class="stars__star fa fa-star stars__star--checked"></span>
+						<span class="stars__star fa fa-star stars__star--checked"></span>
+						<span class="stars__star fa fa-star"></span>
+					</div>
+					<a href="#" class="label label--smaller label--centered label--book-title">Systems Analysis and Design</a>
+				</div>
+				<div class="section__col-4 section__col--centered-content section__col--s-margin">
+					<a href="#">
+						<img src="./assets/images/business-information-systems.jpg" class="img__book--medium">
+					</a>
+					<div class="rating">
+						<span class="stars__star fa fa-star stars__star--checked"></span>
+						<span class="stars__star fa fa-star stars__star--checked"></span>
+						<span class="stars__star fa fa-star"></span>
+						<span class="stars__star fa fa-star"></span>
+						<span class="stars__star fa fa-star"></span>
+					</div>
+					<a href="#" class="label label--smaller label--centered label--book-title">Business Information Systems</a>
+				</div>
+				<div class="section__col-4 section__col--centered-content">
+					<a href="#">
+						<img src="./assets/images/how-to-think-like-a-programmer.jpg" class="img__book--medium">
+					</a>
+					<div class="rating">
+						<span class="stars__star fa fa-star stars__star--checked"></span>
+						<span class="stars__star fa fa-star"></span>
+						<span class="stars__star fa fa-star"></span>
+						<span class="stars__star fa fa-star"></span>
+						<span class="stars__star fa fa-star"></span>
+					</div>
+					<a href="#" class="label label--smaller label--centered label--book-title">How to THINK Like a Programmer</a>
+				</div>
+			
