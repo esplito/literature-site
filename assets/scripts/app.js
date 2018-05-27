b5e24e4e5948782232f5e50a52d89f6d6b852b6c
Emil Esplund <eesplund@gmail.com>
2018-05-27 13:24:44 +0200

    Fixed indentation in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,52 +1,51 @@
+function setStars(current){
+	var counter = 0;
+	$.each($('.modal .stars__star'), function(element, value){
+		if(counter < current){
+			var tempStar = value.getAttribute('data-rating');
+			$('.stars__star[data-rating="' + tempStar + '"]').toggleClass('stars__star--checked');
+			counter++;
+		}
+	});
+}
 
-	function setStars(current){
-		var counter = 0;
-		$.each($('.modal .stars__star'), function(element, value){
-			if(counter < current){
-				var tempStar = value.getAttribute('data-rating');
-				$('.stars__star[data-rating="' + tempStar + '"]').toggleClass('stars__star--checked');
-				counter++;
-			}
-		});
-	}
+function resetStars(){
+	$('.stars__star--checked').toggleClass('stars__star--checked');
+}
 
-	function resetStars(){
-		$('.stars__star--checked').toggleClass('stars__star--checked');
-	}
-
-	function addModal(modal_type){
-		$.get('./overlays/' + modal_type +'.html', null, function(text){
-			$('.modal__inner').html(function(){
-				return text;
-			});
-		});
-	}
-
-	function removeModalContent(){
+function addModal(modal_type){
+	$.get('./overlays/' + modal_type +'.html', null, function(text){
 		$('.modal__inner').html(function(){
-				return "";
+			return text;
 		});
-	}
-
-	var rating = 0;
-
-	$('.modal .stars__star').click(function(e) {
-	    e.preventDefault();
-	    var currentStar = this;
-	    rating = currentStar.getAttribute('data-rating');
-	    resetStars();
-	    setStars(currentStar.getAttribute('data-rating'));
-	    return false;
 	});
+}
 
-	$('.modal__close').click(function(e) {
-		e.preventDefault();
-		$('.modal').toggleClass('modal--visible');
-		removeModalContent();
+function removeModalContent(){
+	$('.modal__inner').html(function(){
+			return "";
 	});
+}
 
-	$('#login-btn').click(function(e){
-		e.preventDefault();
-		addModal("login");
-		$('.modal').toggleClass('modal--visible');
-	});
+var rating = 0;
+
+$('.modal .stars__star').click(function(e) {
+    e.preventDefault();
+    var currentStar = this;
+    rating = currentStar.getAttribute('data-rating');
+    resetStars();
+    setStars(currentStar.getAttribute('data-rating'));
+    return false;
+});
+
+$('.modal__close').click(function(e) {
+	e.preventDefault();
+	$('.modal').toggleClass('modal--visible');
+	removeModalContent();
+});
+
+$('#login-btn').click(function(e){
+	e.preventDefault();
+	addModal("login");
+	$('.modal').toggleClass('modal--visible');
+});
