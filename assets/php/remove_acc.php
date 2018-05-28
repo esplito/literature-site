7370b814c50b5b0dfa5405b92345d1d12b448907
Johan Israelsson <johan4411@gmail.com>
2018-05-28 20:36:29 +0200

    fixed delete for all user levels according to usergrade

--- assets/php/remove_acc.php
+++ assets/php/remove_acc.php
@@ -23,9 +23,8 @@ if ($usersessionlevel == 1) {
       </a>
     </div>
   </div>";
-
-
 }
+
 elseif ($usersessionlevel == 2) {
   while ($roww = mysqli_fetch_array($loweruserlevel)) {
     $rowuname = $roww['user_uname'];
@@ -60,9 +59,59 @@ elseif ($usersessionlevel == 2) {
       </a>
     </div>
   </div>";
-
-
 }
-else {
 
-}
+elseif ($usersessionlevel == 3) {
+    while ($roww = mysqli_fetch_array($loweruserlevel)) {
+      $rowuname = $roww['user_uname'];
+    echo "<div class='user__row section__row--align-l'>
+      <div class='user__image-holder f-left'>
+        <a href='#'>
+          <img class='user__image' src='../assets/images/profile_pic.png'>
+        </a>
+      </div>
+      <div class='user__name f-left'>
+        <p>$rowuname</p>
+      </div>
+      <div class='user__delete f-right'>
+        <a href='../assets/php/user_other_delete.php' class='section__btn-link'>
+          <button name='submit_delete' class='btn btn--small btn--b-text'>Ta bort</button>
+        </a>
+      </div>
+    </div>";}
+
+    while ($roww = mysqli_fetch_array($highuserlevel)) {
+      $rowuname = $roww['user_uname'];
+    echo "<div class='user__row section__row--align-l'>
+      <div class='user__image-holder f-left'>
+        <a href='#'>
+          <img class='user__image' src='../assets/images/profile_pic.png'>
+        </a>
+      </div>
+      <div class='user__name f-left'>
+        <p>$rowuname</p>
+      </div>
+      <div class='user__delete f-right'>
+        <a href='../assets/php/user_other_delete.php' class='section__btn-link'>
+          <button name='submit_delete' class='btn btn--small btn--b-text'>Ta bort</button>
+        </a>
+      </div>
+    </div>";}
+
+  }
+
+    echo "<div class='user__row section__row--align-l'>
+        <div class='user__image-holder f-left'>
+          <a href='#'>
+            <img class='user__image' src='../assets/images/profile_pic.png'>
+          </a>
+        </div>
+        <div class='user__name f-left'>
+          <p>$usersession</p>
+        </div>
+        <div class='user__delete f-right'>
+          <a href='../assets/php/user_session_delete.php' class='section__btn-link'>
+            <button type='submit_delete' name='submit_delete' class='btn btn--small btn--b-text'>Ta bort</button>
+          </a>
+        </div>
+      </div>";
