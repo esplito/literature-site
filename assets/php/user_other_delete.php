c4cc8f9cfab54683ec18352c9342366095449b53
Johan Israelsson <johan4411@gmail.com>
2018-05-28 20:10:09 +0200

    changes header location after user deletion

--- assets/php/user_other_delete.php
+++ assets/php/user_other_delete.php
@@ -3,4 +3,4 @@ session_start();
 include 'remove_acc.php';
 require('connect.php');
 $deleteuser = mysqli_query($connection, "DELETE FROM `user_table` WHERE `user_table`.`user_uname` = '$rowuname'");
-header("Location: ../../pages/profile.php");
+header("Location: ../../pages/removeacc.php");
