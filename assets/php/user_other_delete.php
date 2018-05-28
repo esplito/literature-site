9b596517fd271fa95dc0cca7f8987a2bdfd85fe1
Johan Israelsson <johan4411@gmail.com>
2018-05-28 20:05:46 +0200

    Fixed remove button for other users for moderators and for themselfs

---
+++ assets/php/user_other_delete.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+include 'remove_acc.php';
+require('connect.php');
+$deleteuser = mysqli_query($connection, "DELETE FROM `user_table` WHERE `user_table`.`user_uname` = '$rowuname'");
+header("Location: ../../pages/profile.php");
