7abf5ef33b141b5709f69d413d15829327843aa1
Johanil <32468569+Johanil@users.noreply.github.com>
2018-05-29 03:23:44 +0200

    Fixed profile picture showing for set picture and not set V2

--- assets/php/picture.php
+++ assets/php/picture.php
@@ -3,8 +3,11 @@ require('connect.php');
 $user = $_SESSION['user'];
 $pic = mysqli_query($connection, "SELECT `user_picture` FROM `user_table` WHERE `user_table`.`user_uname` = '$user'");
 $userpic = mysqli_fetch_array($pic);
-if (isset($userpic)) {
-echo "<img class='user__image' src='../assets/images/profile_pic.png'>";
+$emptypic = array_filter($userpic);
+if (!empty($emptypic)) {
+
+echo '<img src="../uploads/'.$userpic['user_picture'].'" class="user__image">';
 } else {
-  echo '<img src="../uploads/'.$userpic['user_picture'].'" class="user__image">';
+  echo "<img class='user__image' src='../assets/images/profile_pic.png'>";
+
 }
