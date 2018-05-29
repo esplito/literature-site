9623b1cf822567436265688cee5fc18c0e784486
TobiasPersson10 <tobias.persson10@gmail.com>
2018-05-29 02:05:03 +0200

    Fixed search function

--- assets/php/Search_Request.php
+++ assets/php/Search_Request.php
@@ -14,13 +14,15 @@ $search = mysqli_real_escape_string($connection, $_POST['search']);
 
 //$books = "SELECT book_title, book_releashed_year FROM book_table WHERE book_title LIKE '%\"('".'$search'."')\"%'";
 
-$books = "SELECT book_title, book_releashed_year FROM book_table WHERE book_title = '$search'";
+$books = "SELECT book_title, book_releashed_year, book_ISBN FROM book_table WHERE book_title = '$search'";
 $result = mysqli_query($connection,$books);
 
 if ($row = $result->fetch_assoc()){
 			$book_title=$row['book_title'];
 			$book_releashed_year=$row['book_releashed_year'];
-			echo $book_title. ", " . $book_releashed_year . '<br />';
+			$book_isbn = $row['book_ISBN'];
+			header("Location: ../../pages/book.php?b=".$book_isbn);
+
 	}
 
 
