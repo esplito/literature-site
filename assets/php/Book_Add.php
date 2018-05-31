364a84b9dc2704b4441faac3463a4f637bcd5e9f
Emil Esplund <eesplund@gmail.com>
2018-05-31 11:53:59 +0200

    Fixed book image upload

--- assets/php/Book_Add.php
+++ assets/php/Book_Add.php
@@ -7,12 +7,14 @@ $title = mysqli_real_escape_string($connection, $_POST['add_title']);
 $author = mysqli_real_escape_string($connection, $_POST['add_author']);
 $edition = mysqli_real_escape_string($connection, $_POST['add_edition']);
 $released_year = mysqli_real_escape_string($connection, $_POST['add_published_year']);
-//$book_picture =  $connection->real_escape_string($_POST['add_book_picture']);
-$imgsbook = $_FILES['file']['name'];
+$imgsbook = basename($_FILES['fileimage']['name']);
 
-$insertbook = mysqli_query($connection, "INSERT INTO book_table (book_ISBN, book_title, book_author, book_edition, book_releashed_year, book_picture) VALUES ('$ISBN','$title','$author','$edition','$released_year', '$imgs')");
-if(mysqli_query($connection, $insertbook)) {
-  move_uploaded_file($_FILES['file']['tmp_name'], "../../uploads/$imgs");
+$insertbook = mysqli_query($connection, "INSERT INTO book_table (book_ISBN, book_title, book_author, book_edition, book_releashed_year, book_picture) VALUES ('$ISBN','$title','$author','$edition','$released_year', '$imgsbook')");
+
+$upload_img = "../../uploads/$imgsbook";
+if($imgsbook != ""){
+	move_uploaded_file($_FILES['fileimage']['tmp_name'], $upload_img);
 }
+
 header("Location: ../../pages/profile.php");
 }
